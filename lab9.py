7d9054110f4812da66c82a32c778068c1f432174
Aaboassoud <a.aboassoud@gmail.com>
2022-05-18 14:25:28 +0300

    ver3

--- lab9.py
+++ lab9.py
@@ -1,15 +1,15 @@
 ## 1) Using recursion, if given a word/phrase return how many vowels(a,e,i,o,u) are in that phrase or word:
-vowels = set("aeiou")
+vowels = ("aeiou")
 def count_vowels(words:str):
     #### Note: the function should be able to count how many vowels no matter if it is lowercase or uppercase . 
     words = words.lower()
-    if words == "":
+    if not words:
         return 0
     elif words[0] in vowels:
         return 1 + count_vowels(words[1:])
     else:
         return 0 + count_vowels(words[1:])
-print(count_vowels("I love python"))
+print("number of vowels in phrase or word = ", count_vowels("I LOVE PYTHON"))
 
 # 2) Given a list of numbers : [40,35, 10, 15, 20]
 list_of_numbers = [40,35, 10, 15, 20]
